123539c619742ec23712b0e59629251922db0457
woongi9 <woongi9@naver.com>
2022-03-26 01:08:19 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -0,0 +1,38 @@
+package hello.itemservice.web;
+
+
+import hello.itemservice.domain.member.Member;
+import hello.itemservice.domain.member.MemberRepository;
+import hello.itemservice.web.session.SessionManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
+
+
+    @GetMapping("/")
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+}
